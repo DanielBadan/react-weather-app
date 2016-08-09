60b9252c51564c067fa46b33d33f4e37ce76d02e
danielverejan <danie.verejan@gmail.com>
2016-08-09 17:08:01 +0300

    Done step 8

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,12 +1,70 @@
 import React, {PropTypes} from 'react';
+import {getDate} from '../helpers/utils';
+
+var styles = {
+	container: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		maxWidth: 1200,
+		margin: '50px auto'
+	},
+	dayContainer: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		flexDirection: 'column',
+		margin: 35
+	},
+	header: {
+		fontSize: 65,
+		color: '#333',
+		fontWeight: 100,
+		textAlign: 'center'
+	},
+	subHeader: {
+		fontSize: 30,
+		color: '#333',
+		fontWeight: 100
+	},
+	weather: {
+		height: 130
+	}
+}
+
+const DayItem = (props) => {
+	var date = getDate(props.day.dt);
+	var icon = props.day.weather[0].icon;
+
+	return (
+		<div style={styles.dayContainer}>
+			<img src={`./app/images/weather-icons/${icon}.svg`} alt="Weather"/>
+			<h2 style={styles.subHeader}>{date}</h2>
+		</div>
+	);
+};
+
+const ForecastUi = (props) => (
+	<div>
+		<h1 style={styles.header}>{props.city}</h1>
+		<div style={styles.container}>
+			{props.forecast.list.map(item => <DayItem key={item.dt} day={item} />)}
+		</div>
+	</div>
+);
 
 const Forecast = (props) => {
 	console.log(props);
 	return (
 		<div>
 			{ props.isLoading ? 
-				<h2 className="loading">Loading</h2> :
-				<h2 className="loading">Forecast component</h2>
+				<h2 style={styles.header}>Loading</h2> :
+				<ForecastUi 
+					forecast={props.forecastData}
+					city={props.city}
+				/>
 			}
 		</div>
 	)
